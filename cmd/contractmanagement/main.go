d32348e4dff7abca54fbd13a1148c7a555f398a0
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 04:11:09 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/contractmanagement/main.go
@@ -0,0 +1,23 @@
+// cmd/contractmanagement/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "contractmanagement/internal/contractmanagement"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := contractmanagement.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
